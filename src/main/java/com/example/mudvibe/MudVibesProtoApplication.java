b2aa7607f64159ec33b899457f19009106aadad8
robert <robert@pop-os.localdomain>
2026-03-27 00:03:26 -0400

    Initial commit of Project!"

---
+++ src/main/java/com/example/mudvibe/MudVibesProtoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mudvibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudVibesProtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudVibesProtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mudvibe/MudVibesProtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mudvibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudVibesProtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
